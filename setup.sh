c1f99cd7f7ab16536c8951bf63b4dc0a158c6f95
Fernando Alexandre <jfernandoalex@gmail.com>
2019-12-15 22:20:18 +0000

    correcting typo

--- setup.sh
+++ setup.sh
@@ -25,7 +25,7 @@ brew install macvim
 $(brew --prefix)/opt/fzf/install
 
 # Pathogen plugins
-git@github.com:tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
+git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
 git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 git clone https://github.com/airblade/vim-gitgutter ~/.vim/bundle/vim-gitgutter
 git clone https://github.com/altercation/vim-colors-solarized ~/.vim/bundle/vim-colors-solarized
