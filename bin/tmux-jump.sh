d2f144c6acc707e0bff8b0762227772d5a54d370
fernandoalex <jfernandoalex@gmail.com>
2022-06-20 10:18:10 +0100

    adding preview to fzf
    Signed-off-by: fernandoalex <jfernandoalex@gmail.com>

--- bin/tmux-jump.sh
+++ bin/tmux-jump.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/git ~/OSS -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find ~/git ~/OSS -mindepth 1 -maxdepth 1 -type d | fzf --preview 'exa -l {}')
 fi
 
 if [[ -z $selected ]]; then
