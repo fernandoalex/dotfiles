ee1b67b75a87ffbbb010a5077222837fc2b89fc7
fernandoalex <jfernandoalex@gmail.com>
2022-09-13 23:31:39 +0100

    comments and formating
    Signed-off-by: fernandoalex <jfernandoalex@gmail.com>

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -1,7 +1,7 @@
--- [[ Basic Keymaps ]]
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+-- [[ basic keymaps ]]
+-- set <space> as the leader key
+-- see `:help mapleader`
+--  NOTE: must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
@@ -13,7 +13,7 @@ vim.opt.clipboard = "unnamedplus" -- share clipboard with system
 vim.keymap.set('i', '<C-j>', '<Esc>', { desc = '[?] Ctrl-J to Esc' })
 vim.keymap.set('n', '<leader><leader>', '<c-^>', { desc = '[?] Open last file' })
 
--- basic plugins with no/simple custom config
+-- [[ basic plugins with no/simple custom config ]]
 require("substitute").setup()
 vim.keymap.set("n", "s", "<cmd>lua require('substitute').operator()<cr>", { noremap = true })
 vim.keymap.set("x", "s", "<cmd>lua require('substitute').visual()<cr>", { noremap = true })
