23b15c4067f90d97277e52d941ef0babe7ecdae3
fernandoalex <jfernandoalex@gmail.com>
2023-04-23 11:12:53 +0100

    stuff
    Signed-off-by: fernandoalex <jfernandoalex@gmail.com>

--- home.nix
+++ home.nix
@@ -1,5 +1,3 @@
-# TODOs:
-# - NVIDIA Cuda stuff
 { config, pkgs, ... }:
 
 {
@@ -7,14 +5,16 @@
   home.homeDirectory = "/home/spanta";
 
   home.stateVersion = "22.11"; # Please read the comment before changing.
-
   home.packages = with pkgs; [
     neovim
+    neomutt
     tmux
     fzf
     fd
     ripgrep
     bat
+    exa
+    gh
     go
     nodejs
     unzip
@@ -29,6 +29,13 @@
     alsaLib
     alsaLib.dev
     starship
+    zoxide
+    zsh
+    lynx
+    firefox
+    awscli
+    direnv
+    zstd
   ];
 
   xdg.configFile.nvim = {
@@ -36,53 +43,48 @@
     recursive = true;
   };
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
-  # You can also manage environment variables but you will have to manually
-  # source
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/spanta/etc/profile.d/hm-session-vars.sh
-  #
-  # if you don't want to manage your shell through Home Manager.
   home.sessionVariables = {
-    EDITOR = "nvim";
-    PKG_CONFIG_PATH = "${config.home.profileDirectory}/lib/pkgconfig";
+	  EDITOR = "nvim";
+	  PKG_CONFIG_PATH = "${config.home.profileDirectory}/lib/pkgconfig";
   };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  programs.tmux = {
+	enable = true;
+	shortcut = "a";
+	extraConfig = ''
+		set -g default-terminal "xterm-256color"
+		set -ga terminal-overrides ",*256col*:Tc"
+		set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
+		set-environment -g COLORTERM "truecolor"
+
+		bind _ split-window -h -c "#{pane_current_path}"
+		bind - split-window -v -c "#{pane_current_path}"
+		bind c new-window -c "#{pane_current_path}"
+    '';
+
+  };
+
+  programs.zsh = {
+	enable = true;
+	initExtra = ''
+		eval "$(zoxide init zsh)"
+	'';
+  };
+  
+
+  # starship stuff
   programs.starship.enable = true;
   programs.starship.settings = {
 	  add_newline = false;
-	  format = "$shlvl$shell$username$hostname$nix_shell$git_branch$git_commit$git_state$git_status$directory$jobs$cmd_duration\n$character";
-	  shlvl = {
-		  disabled = false;
-		  symbol = "ﰬ";
-		  style = "bright-red bold";
-	  };
+	  format = 
+	  "$username$hostname$nix_shell$git_branch$git_commit$git_state$git_status$directory$jobs$cmd_duration\n$character";
 	  shell = {
 		  disabled = false;
 		  format = "$indicator";
-		  fish_indicator = "";
-		  bash_indicator = "[BASH](bright-white) ";
-		  zsh_indicator = "[ZSH](bright-white) ";
 	  };
 	  username = {
 		  style_user = "bright-white bold";
